67decb0bef8ac4e2cbc9855b18744b99d224df7f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-02 02:08:05 +0300

    Create airport model and migrate database

---
+++ db/migrate/20250901230545_create_airports.rb
@@ -0,0 +1,10 @@
+class CreateAirports < ActiveRecord::Migration[8.0]
+  def change
+    create_table :airports do |t|
+      t.string :airport_name
+      t.string :airport_code
+
+      t.timestamps
+    end
+  end
+end
