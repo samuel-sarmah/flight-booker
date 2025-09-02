b7ba910374ba5124a62cd965013a3c6b41d6984a
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-02 08:19:57 +0300

    Reverse controller generation to fix typo

--- app/controllers/flight_controller.rb
+++
@@ -1,2 +0,0 @@
-class FlightController < ApplicationController
-end
--- test/controllers/flight_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class FlightControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
