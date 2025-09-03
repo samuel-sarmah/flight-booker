595949179e3c2533fe92cb9fe779df42d8f57a98
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-04 02:23:26 +0300

    Set up index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,14 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |airport| [ airport.airport_name, airport.airport_code ] }
+    @date_options = Flight.select(:startdatetime).map { |f| f.startdatetime.to_date }.uniq.sort
+    @passenger_options = (1..4)
+
+    @flights = Flight.all
+    if params[:search]
+      @flight = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
+      @flight = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+      @flights = @flights.where("Date(startdatetime) = ?", params[:startdatetime]) if params[:startdatetime].present?
+    end
   end
 end
