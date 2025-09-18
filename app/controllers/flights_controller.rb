b5fd564988b23ff9ec2a995ee84af8fbcb3c06c0
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-18 05:11:57 +0300

    Update index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,28 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |airport| [ airport.airport_name, airport.airport_code ] }
+    @airport_options = Airport.all.map { |airport|  [ airport.airport_code, airport.id ] }
     @passenger_options = (1..4)
 
     @flights = Flight.all
-    if params[:search]
-      @flight = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
-      @flight = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
-      @flights = @flights.where("Date(startdatetime) = ?", params[:startdatetime]) if params[:startdatetime].present?
+
+    if params[:search].present?
+      if params[:departure_airport_id].present?
+        @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
+      end
+
+      if params[:arrival_airport_id].present?
+        @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id])
+      end
+
+      if params[:startdatetime].present?
+        @flights = @flights.where("DATE(startdatetime) = ?", params[:date])
+      end
     end
+  end
+
+  private
+
+  def search_params
+    params.permit(:departure_airport_id, :arrival_airport_id, :startdatetime)
   end
 end
