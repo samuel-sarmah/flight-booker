ad0c41cc497ace79ae5087cfb2f8d3af419ba91f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-18 05:12:41 +0300

    Add validations to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
+
+  validates :departure_airport, :arrival_airport, :startdatetime, presence: true
 end
