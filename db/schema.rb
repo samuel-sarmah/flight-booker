9213b72c9042619dc055bff8a9e3374dfe282018
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-18 15:32:52 +0300

    Change to postgresql database from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_18_114710) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "airports", force: :cascade do |t|
     t.string "airport_name"
     t.string "airport_code"
