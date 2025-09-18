7fe9621da5adfb9ded2557f2a205d6978b9a4132
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-18 17:37:48 +0300

    Implement the create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,8 +7,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: "Your booking has been successfull!"
+    else
+      @flight = booking.flight
+      @passenger_count = @booking.passengers.size
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  def booking_params
+    params.reequire(:booking).permit(:flight_id, passenger_attributes: [ :name, :email ])
   end
 end
