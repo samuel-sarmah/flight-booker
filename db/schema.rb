78e5918d1ddd908dac87e5c4cba3c16950824173
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-09-18 05:59:12 +0300

    Create and migrate booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_02_045525) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_18_025823) do
   create_table "airports", force: :cascade do |t|
     t.string "airport_name"
     t.string "airport_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
