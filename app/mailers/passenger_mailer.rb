c4521b03b9050c193f3671248fd51a888725590c
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-20 14:59:49 +0300

    feat: add ticket number generator

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,10 +1,11 @@
 class PassengerMailer < ApplicationMailer
-  default from: "notifications@flight-booker.com"
+  default from: "sarmah@flight-booker.com"
 
   def confirmation_email
     @passenger = params[:passenger]
     @booking = params[:booking]
     @flight = @booking.flight
+    @ticket_number = @passenger.ticket_number
 
     mail(to: @passenger.email,
          subject: "Flight Booking Confirmation - #{@flight.departure_airport.airport_code} to #{@flight.arrival_airport.airport_code}")
