5e140d4d7bfb3c3acc0be3cf4b67fb85b0628a67
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-10-20 12:53:44 +0300

    feat: create action to send confirmation email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: "notifications@flight-booker.com"
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: "You booking has been confirmed")
+  end
 end
