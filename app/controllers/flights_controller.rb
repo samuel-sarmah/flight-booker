cf7514eb1f223f86eccf8e7a5ea476c6a29aa6be
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-01-08 10:19:59 +0200

    fix: Permit passengers param, show alternatives on no results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,14 +2,14 @@ class FlightsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def index
-    @airport_options = Airport.all.order(:airport_code).map do |airport| 
-      ["#{airport.airport_code} - #{airport.airport_name}", airport.id]
+    @airport_options = Airport.all.order(:airport_code).map do |airport|
+      [ "#{airport.airport_code} - #{airport.airport_name}", airport.id ]
     end
-    @passenger_options = (1..9).map { |n| [pluralize(n, "passenger"), n] }
+    @passenger_options = (1..9).map { |n| [ pluralize(n, "passenger"), n ] }
 
     if search_params.present?
       @flights = Flight.includes(:departure_airport, :arrival_airport)
-      
+
       if params[:departure_airport_id].present?
         @flights = @flights.where(departure_airport_id: params[:departure_airport_id])
       end
@@ -23,6 +23,14 @@ class FlightsController < ApplicationController
       end
 
       @flights = @flights.order(:startdatetime).limit(20)
+      
+      # If no flights match, show available alternatives
+      if @flights.empty?
+        @available_flights = Flight.includes(:departure_airport, :arrival_airport)
+                                   .where("startdatetime >= ?", Time.current)
+                                   .order(:startdatetime)
+                                   .limit(10)
+      end
     else
       @flights = []
     end
@@ -31,6 +39,6 @@ class FlightsController < ApplicationController
   private
 
   def search_params
-    params.permit(:departure_airport_id, :arrival_airport_id, :startdatetime)
+    params.permit(:departure_airport_id, :arrival_airport_id, :startdatetime, :passengers, :commit)
   end
 end
